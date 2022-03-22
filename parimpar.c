a7dfa1232102f3918d75ef9b451b0d49a3b26c7e
Maressaa <102029411+Maressaa@users.noreply.github.com>
2022-03-22 07:55:35 -0300

    Add files via upload

--- parimpar.c
+++ parimpar.c
@@ -4,5 +4,17 @@ int main () {
     printf("Escola Senai Euclides Facchini  Votuporanga \n");
     printf("Maressa dos Santos Gonçalves\n");
 
+    int a;
+    int b;
 
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &a);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &b);
+  
+    for (int i = a; i<=b;i++){
+        if(i % 2==0) {
+            printf("O par e :%d\n", i);
+        }
+    }
 }
